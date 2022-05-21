130bc114fae5474bf7fc0610fe9d38c6bc2c68b7
jeawook <pjw4825@naver.com>
2022-05-21 17:14:09 +0900

    commit project

--- src/headfirst/designpatterns/decorator/pizza/Pizza.java
+++ src/headfirst/designpatterns/decorator/pizza/Pizza.java
@@ -1,11 +1,33 @@
 package headfirst.designpatterns.decorator.pizza;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Pizza {
-	String description = "Basic Pizza";
-  
-	public String getDescription() {
-		return description;
+
+	String name;
+	String dough;
+	String sauce;
+	List<String> toppings = new ArrayList<>();
+	void prepare() {
+		System.out.println("준비중 : " + name);
+		System.out.println("도우를 돌리는 중....");
+		System.out.println("소스를 뿌리는 중....");
+		System.out.println("토핑을 올리는 중 :");
+		for (String topping : toppings) {
+			System.out.println(" " +topping);
+		}
 	}
- 
-	public abstract double cost();
+	void bake() {
+		System.out.println("175도에서 25분간 굽기");
+	}
+	void cut() {
+		System.out.println("피자를 사선으로 자르기");
+	}
+	void box() {
+		System.out.println("상자에 피자를 담기");
+	}
+	public String getName() {
+		return name;
+	}
 }
---
+++ src/headfirst/designpatterns/decorator/pizza/PizzaTestDrive.java
@@ -0,0 +1,8 @@
+package headfirst.designpatterns.decorator.pizza;
+
+public class PizzaTestDrive {
+    public static void main(String[] args) {
+        PizzaStore nyStore = new NYStyleCheesePizza();
+        
+    }
+}
